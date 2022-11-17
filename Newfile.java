55f2393b1beb58f7d36e90c336c18d1cc70b4b05
souravkh <souravkhatri1996@gmail.com>
2022-11-17 12:39:07 +0530

    updated

--- Newfile.java
+++ Newfile.java
@@ -1,5 +1,12 @@
 class Newfile{
 public static void main( String[] args){
 	System.out.println("Hello !");
+
+for(int i=1;i<=3;i++){
+	for(int j=1;j<=i;j++){
+		System.out.print("*");
+		}
+	System.out.println("");	
+	}
 }
 }
